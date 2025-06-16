1bd5b5d6929f95a8adc5941e830ad21b09460fd3
DANDITFQ\Danditfq <danditaufik23@gmail.com>
2025-06-16 19:34:21 +0700

    Tambah Filter & pindah css loginregister

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,27 +9,26 @@ use CodeIgniter\Router\RouteCollection;
 // Halaman utama
 $routes->get('/', 'Home::index');
 
-// Login
+// Login & Register (tanpa filter)
 $routes->get('/login', 'Login::index');
 $routes->post('/login_action', 'Login::login_action');
 $routes->get('/logout', 'Login::logout');
 
-// Register
 $routes->get('/register', 'Register::index');
 $routes->post('/register_action', 'Register::proses');
 
+$routes->group('admin', ['filter' => 'auth'], function($routes) {
+    $routes->get('index', 'Admin::index');
+    $routes->get('data_barang', 'Admin::dataBarang');
+});
 
-// Admin
-$routes->get('/admin/index', 'Admin::index');
-$routes->get('/admin/data_barang', 'Admin::dataBarang');
+$routes->group('pegawai', ['filter' => 'auth'], function($routes) {
+    $routes->get('dashboard', 'Pegawai::index');
+    $routes->get('data_barang', 'Barang::index');
+    $routes->get('create_barang', 'Barang::create');
+    $routes->post('save_barang', 'Barang::store');
+    $routes->get('edit_barang/(:num)', 'Barang::edit/$1');
+    $routes->post('update_barang/(:num)', 'Barang::update/$1');
+    $routes->post('delete_barang/(:num)', 'Barang::delete/$1');
+});
 
-// Pegawai
-$routes->get('/pegawai/dashboard', 'Pegawai::index');
-
-// Barang (fitur untuk pegawai)
-$routes->get('/pegawai/data_barang', 'Barang::index');
-$routes->get('/pegawai/create_barang', 'Barang::create');
-$routes->post('/pegawai/save_barang', 'Barang::store');
-$routes->get('/pegawai/edit_barang/(:num)', 'Barang::edit/$1');
-$routes->post('/pegawai/update_barang/(:num)', 'Barang::update/$1');
-$routes->post('/pegawai/delete_barang/(:num)', 'Barang::delete/$1');
