14745a1102e6a9858c529e9ed79a24b48d28a44e
LAPTOP-0UHCDBEE\Wulan <sriakmaliatulmaulani05@gmail.com>
2025-06-18 22:12:24 +0700

    profil

--- app/Views/pegawai/profil.php
+++ app/Views/pegawai/profil.php
@@ -7,8 +7,24 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
+<<<<<<< HEAD
         body { background-color: #f8f9fa; }
         .profile-card { max-width: 600px; margin: auto; border-radius: 1rem; }
+=======
+        body {
+            background: linear-gradient(135deg,rgb(255, 255, 255),rgb(101, 185, 254));
+            font-family: 'Segoe UI', sans-serif;
+        }
+
+        .profile-card {
+            background: white;
+            border-radius: 16px;
+            box-shadow: 0 8px 24px rgba(0,0,0,0.15);
+            padding: 30px;
+            margin-top: 50px;
+        }
+
+>>>>>>> 97215e4 (profil)
         .profile-header {
             background-color: #0d6efd;
             color: white;
